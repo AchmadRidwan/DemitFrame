23b4dac65577cb386ace98e96044eaa236c4aa3e
quantumpanic <Hambali.Hamdan@matatacorp>
2016-09-28 12:21:12 +0700

    Revert "l"
    This reverts commit bded088e4a8ef4d3eb67500f5f67f47ffd6d75d9.

--- DemitFrame/src/demitapplication/JSon.java
+++ DemitFrame/src/demitapplication/JSon.java
@@ -2523,8 +2523,6 @@ public class JSon extends javax.swing.JFrame {
         }
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\resource\\produsen\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -2601,8 +2599,6 @@ public class JSon extends javax.swing.JFrame {
         }
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\resource\\storage\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -2716,8 +2712,6 @@ public class JSon extends javax.swing.JFrame {
         }
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\defenses\\buildings\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -2827,8 +2821,6 @@ public class JSon extends javax.swing.JFrame {
         }
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\defenses\\traps\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -2897,8 +2889,6 @@ public class JSon extends javax.swing.JFrame {
         int maximumUnit = getInt(maximumUnitAr);
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\army buildings\\produsen\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -2976,8 +2966,6 @@ public class JSon extends javax.swing.JFrame {
         int troopsCapacity = getInt(troopCapacityAr);
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\army buildings\\produsen\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -3062,8 +3050,6 @@ public class JSon extends javax.swing.JFrame {
         int unitType = getInt(unitTypeArmy);
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\army\\"+unitType+"-"+sName+"-"+iLevel+".json");
 
             if(fa.exists()){
@@ -3107,7 +3093,7 @@ public class JSon extends javax.swing.JFrame {
                     e.printStackTrace();
                 }
     }//GEN-LAST:event_jButton7ActionPerformed
- int damage,speed,hasil ;
+ int  damage,speed,hasil ;
  int damageD,speedDef,hasilDef ;
     private void damageAttackArmyFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_damageAttackArmyFocusLost
         // TODO add your handling code here:
@@ -3239,10 +3225,7 @@ public class JSon extends javax.swing.JFrame {
         }
 
         String dir = System.getProperty("user.dir");
-        Path currentRelativePath = Paths.get("");
-        dir = currentRelativePath.toAbsolutePath().toString();
         File fa = new File(dir+"\\unit kategory\\buildings\\resource\\pusat\\"+unitType+"-"+sName+"-"+iLevel+".json");
-        System.out.println(fa);
 
             if(fa.exists()){
                 int Ex = JOptionPane.showConfirmDialog(this, "Timpa file?");
